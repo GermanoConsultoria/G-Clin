9cb34ac2fa94596188f57a8fc809c53400655e8a
GermanoConsultoria <agendagermanoconsultoria@gmail.com>
2026-06-15 14:03:52 -0300

    fix: hardcode supabase vars seguindo padrão BuratinOutlet

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   },
   vite: {
     define: {
-      "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(process.env.VITE_SUPABASE_URL),
-      "import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY": JSON.stringify(process.env.VITE_SUPABASE_PUBLISHABLE_KEY),
+      "import.meta.env.VITE_SUPABASE_URL": JSON.stringify("https://otjzxrwuzkugpfwnyuox.supabase.co"),
+      "import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY": JSON.stringify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90anp4cnd1emt1Z3Bmd255dW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NTQxNTMsImV4cCI6MjA5NjAzMDE1M30.WXW2h_GiR75QxqFSLTDSC1wTjyjVEY-GzmybXfcdp5w"),
     },
   },
 });
